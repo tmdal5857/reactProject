834f31e601adc57cc5922212478e15cb98c68097
tmdal5857 <63244032+tmdal5857@users.noreply.github.com>
2020-09-29 20:08:58 +0900

    Add Get Customer's REST API module

--- react/management/client/src/App.js
+++ react/management/client/src/App.js
@@ -19,33 +19,24 @@ const styles = {
   }
 };
 
-const customers = [
-{
-  'id':'1',
-  'image': 'https://placeimg.com/64/64/1',
-  'name':'홍길동',
-  'birthday':'961122',
-  'gender':'남자',
-  'job':'대학생'
- },
- {
-  'id':'2',
-  'image': 'https://placeimg.com/64/64/2',
-  'name':'홍길동',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
- },
- {
-  'id':'3',
-  'image': 'https://placeimg.com/64/64/3',
-  'name':'홍길동',
-  'birthday':'961122',
-  'gender':'여자',
-  'job':'대학생'
- }
-]
 class App extends Component{
+
+  state = {
+    customers:""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  } 
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const { classes} = this.props;
   return (
@@ -62,7 +53,7 @@ class App extends Component{
       </TableRow>
     </TableHead>
     <TableBody>
-      {customers.map(c => {
+      {this.state.customers ? this.state.customers.map(c => {
         return <Customer
             id={c.id}
             image={c.image}
@@ -70,7 +61,7 @@ class App extends Component{
             birthday={c.birthday}
             gender={c.gender}
             job={c.job}/>
-      })}
+      }) : ""}
     </TableBody>
   </Table>
   </Paper>
